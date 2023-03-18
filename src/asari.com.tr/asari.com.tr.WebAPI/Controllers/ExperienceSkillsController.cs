866e4827efdd9aba435b5cb0ba457eca014dd945
alisari41 <alisari41@outlook.com>
2023-03-18 20:50:37 +0300

    Experience Skill - Get List işlemleri tamamlandı.

---
+++ src/asari.com.tr/asari.com.tr.WebAPI/Controllers/ExperienceSkillsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asari.com.tr.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExperienceSkillsController : ControllerBase
+    {
+    }
+}
