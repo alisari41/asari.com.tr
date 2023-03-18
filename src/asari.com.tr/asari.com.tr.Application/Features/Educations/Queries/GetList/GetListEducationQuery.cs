1e5b5b25930407a3cea0b6c8355d2cb7b187aeb0
alisari41 <alisari41@outlook.com>
2023-03-19 00:02:46 +0300

    Education - Get List işlemleri tamamlandı.

---
+++ src/asari.com.tr/asari.com.tr.Application/Features/Educations/Queries/GetList/GetListEducationQuery.cs
@@ -0,0 +1,34 @@
+﻿using asari.com.tr.Application.Services.Repositories;
+using asari.com.tr.Domain.Entities;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using MediatR;
+
+namespace asari.com.tr.Application.Features.Educations.Queries.GetList;
+
+public class GetListEducationQuery : IRequest<GetListResponse<GetListEducationListItemDto>>
+{
+    public PageRequest PageRequest { get; set; } // Bir listeleme yapılacağı için bir Request üzerinden geçekleştirilecek
+
+    public class GetListEducationQueryHandler : IRequestHandler<GetListEducationQuery, GetListResponse<GetListEducationListItemDto>>
+    {
+        private readonly IEducationRepository _educationRepository;
+        private readonly IMapper _mapper;
+
+        public GetListEducationQueryHandler(IEducationRepository educationRepository, IMapper mapper)
+        {
+            _educationRepository = educationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetListResponse<GetListEducationListItemDto>> Handle(GetListEducationQuery request, CancellationToken cancellationToken)
+        {
+            IPaginate<Education> education = await _educationRepository.GetListAsync(index: request.PageRequest.Page, size: request.PageRequest.PageSize);
+
+            GetListResponse<GetListEducationListItemDto> mappedGetListEducationListItemDto = _mapper.Map<GetListResponse<GetListEducationListItemDto>>(education);
+
+            return mappedGetListEducationListItemDto;
+        }
+    }
+}
---
+++ src/asari.com.tr/asari.com.tr.WebAPI/Controllers/EducationsController.cs
@@ -0,0 +1,20 @@
+﻿using asari.com.tr.Application.Features.Educations.Queries.GetList;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asari.com.tr.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EducationsController : BaseController
+{
+    [HttpGet("get-list")]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListEducationQuery getListEducationQuery = new() { PageRequest = pageRequest };
+
+        GetListResponse<GetListEducationListItemDto> result = await Mediator.Send(getListEducationQuery);
+        return Ok(result);
+    }
+}
