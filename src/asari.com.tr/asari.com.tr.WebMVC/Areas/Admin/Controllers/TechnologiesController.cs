d6c0b790ca0904b2acf34e08dbf808f88575a997
alisari41 <alisari41@outlook.com>
2023-05-21 14:36:23 +0300

    MVC - AREAS - Technologies - GETLİST - Add - Update - Delete - PartialView - GetList işlemleri tamamlandı.

---
+++ src/asari.com.tr/asari.com.tr.WebMVC/Areas/Admin/Controllers/TechnologiesController.cs
@@ -0,0 +1,196 @@
+﻿using asari.com.tr.Application.Features.Technologies.Commands.Update;
+using asari.com.tr.Application.Features.Technologies.Queries.GetById;
+using asari.com.tr.Application.Features.Technologies.Commands.Create;
+using asari.com.tr.Application.Features.Technologies.Queries.GetList;
+using Core.Application.Requests;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using Core.Persistence.Paging;
+using Microsoft.AspNetCore.Mvc;
+using asari.com.tr.Application.Features.Technologies.Commands.Delete;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace asari.com.tr.WebMVC.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class TechnologiesController : BaseController
+{
+    [HttpGet("/Technologies/GetList")]
+    public async Task<IActionResult> GetList(PageRequest pageRequest)
+    {
+        try
+        {
+            // Sayfa boyutu ve sayfa sayısı hesaplanır. 
+            pageRequest.Page = pageRequest.Page != 0 ? pageRequest.Page : 0;
+            pageRequest.PageSize = pageRequest.PageSize != 0 ? pageRequest.PageSize : 15;
+
+            GetListTechnologyQuery getListTechnologyQuery = new() { PageRequest = pageRequest };
+
+            GetListResponse<GetListTechnologyListItemDto> result = await Mediator.Send(getListTechnologyQuery);
+
+            // Verileri görüntülemek için view'e gönderilir
+            return View(result);
+        }
+        catch (Exception exception)
+        {
+            ViewBag.ExceptionErrorMessage = exception.Message;
+            ViewBag.ExceptionErrorStackTrace = exception.StackTrace;
+
+            return View();
+        }
+    }
+
+    [HttpGet("/Technologies/GetListDigerTablar")]
+    public async Task<IActionResult> GetListDigerTablar(PageRequest pageRequest)
+    {
+        try
+        {
+            // Sayfa boyutu ve sayfa sayısı hesaplanır. 
+            pageRequest.Page = pageRequest.Page != 0 ? pageRequest.Page : 0;
+            pageRequest.PageSize = pageRequest.PageSize != 0 ? pageRequest.PageSize : 15;
+
+            GetListTechnologyQuery getListTechnologyQuery = new() { PageRequest = pageRequest };
+
+            GetListResponse<GetListTechnologyListItemDto> result = await Mediator.Send(getListTechnologyQuery);
+
+            // Verileri görüntülemek için view'e gönderilir
+            return View(result);
+        }
+        catch (Exception exception)
+        {
+            ViewBag.ExceptionErrorMessage = exception.Message;
+            ViewBag.ExceptionErrorStackTrace = exception.StackTrace;
+
+            return View();
+        }
+    }
+
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost("/Technologies/Add")]
+    public async Task<IActionResult> Add(CreateTechnologyCommand createTechnologyCommand)
+    {
+        try
+        {
+            CreatedTechnologyResponse result = await Mediator.Send(createTechnologyCommand); // Command'i de Madiator aracığılıyla handler'ını bulması için görevlendiriyoruz.
+            //ViewBag.Success = "Kaydetme İşlemi Başarılı";
+
+            return RedirectToAction("GetList");
+        }
+        catch (AuthorizationException authorizationException)
+        {
+            ViewBag.AuthorizationErrorMessage = authorizationException.Message;
+            ViewBag.AuthorizationErrorStackTrace = authorizationException.StackTrace;
+
+            return View();
+        }
+        catch (BusinessException businessException)
+        {
+            ViewBag.BusinessErrorMessage = businessException.Message;
+            ViewBag.BusinessErrorStackTrace = businessException.StackTrace;
+
+            return View();
+        }
+        catch (NotFoundException notFoundException)
+        {
+            ViewBag.NotFoundErrorMessage = notFoundException.Message;
+            ViewBag.NotFoundErrorStackTrace = notFoundException.StackTrace;
+
+            return View();
+        }
+        catch (ValidationException validationException)
+        {
+            ViewBag.ValidationErrorMessage = validationException.Message;
+            ViewBag.ValidationErrorStackTrace = validationException.StackTrace;
+
+            return View();
+        }
+        catch (Exception exception)
+        {
+            ViewBag.ExceptionErrorMessage = exception.Message;
+            ViewBag.ExceptionErrorStackTrace = exception.StackTrace;
+
+            return View();
+        }
+    }
+
+    public async Task<IActionResult> Update(GetByIdTechnologyQuery getByIdTechnologyQuery)
+    {
+        GetByIdTechnologyResponse result = await Mediator.Send(getByIdTechnologyQuery);
+
+        UpdateTechnologyCommand updateTechnologyCommand = new UpdateTechnologyCommand
+        { // Update metodundan geriye sadece Result döndürdüğümüzde hata vermektedir.
+            Id = result.Id,
+            Title = result.Title,
+            Description = result.Description,
+            ImageUrl = result.ImageUrl,
+            Content = result.Content
+        };
+
+        return View(updateTechnologyCommand);
+    }
+
+    [HttpPost("/Technologies/Update")]
+    public async Task<IActionResult> Update(UpdateTechnologyCommand updateTechnologyCommand)
+    {
+        try
+        {
+            UpdatedTechnologyResponse result = await Mediator.Send(updateTechnologyCommand);
+            return RedirectToAction("GetList");
+        }
+        catch (AuthorizationException authorizationException)
+        {
+            ViewBag.AuthorizationErrorMessage = authorizationException.Message;
+            ViewBag.AuthorizationErrorStackTrace = authorizationException.StackTrace;
+
+            return View(updateTechnologyCommand); // Hata MEsajı aldığımda geriye updateTechnologyCommand'i döndürmezsem Form içerisinde @Model.Id boş muş gibi hata veriyor
+        }
+        catch (BusinessException businessException)
+        {
+            ViewBag.BusinessErrorMessage = businessException.Message;
+            ViewBag.BusinessErrorStackTrace = businessException.StackTrace;
+
+            return View(updateTechnologyCommand);
+        }
+        catch (NotFoundException notFoundException)
+        {
+            ViewBag.NotFoundErrorMessage = notFoundException.Message;
+            ViewBag.NotFoundErrorStackTrace = notFoundException.StackTrace;
+
+            return View(updateTechnologyCommand);
+        }
+        catch (ValidationException validationException)
+        {
+            ViewBag.ValidationErrorMessage = validationException.Message;
+            ViewBag.ValidationErrorStackTrace = validationException.StackTrace;
+
+            return View(updateTechnologyCommand);
+        }
+        catch (Exception exception)
+        {
+            ViewBag.ExceptionErrorMessage = exception.Message;
+            ViewBag.ExceptionErrorStackTrace = exception.StackTrace;
+
+            return View(updateTechnologyCommand);
+        }
+    }
+
+    [HttpPost("/Technologies/Delete")]
+    public async Task<IActionResult> Delete(DeleteTechnologyCommand deleteTechnologyCommand)
+    {
+        DeletedTechnologyResponse result = await Mediator.Send(deleteTechnologyCommand);
+        return RedirectToAction("GetList");
+    }
+
+    [AllowAnonymous]
+    public IActionResult Logout()
+    {
+        HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        HttpContext.Session.Clear();
+        return Redirect("/");
+    }
+}
